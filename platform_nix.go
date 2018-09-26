9666956a6c4d86b133cb54f620336bb05fc065e8
ardnew <andrew@ardnew.com>
2018-09-26 00:17:19 -0500

    add: added standard source file header

--- platform_nix.go
+++ platform_nix.go
@@ -1,5 +1,19 @@
 // +build linux darwin
 
+// =============================================================================
+//  PROJ: pimmp
+//  AUTH: ardnew
+//  DATE: 26 Sept 2018
+//  FILE: platform_nix.go
+// -----------------------------------------------------------------------------
+//
+//  DESCRIPTION
+//    all data related to system-dependent operations can be stored here
+//    unconditionally. the Go compiler takes care of identifying which of the
+//    symbols to export based on whichever platform we are targeting.
+//
+// =============================================================================
+
 package main
 
 const (
--- platform_win.go
+++ platform_win.go
@@ -1,5 +1,19 @@
 // +build windows
 
+// =============================================================================
+//  PROJ: pimmp
+//  AUTH: ardnew
+//  DATE: 26 Sept 2018
+//  FILE: platform_win.go
+// -----------------------------------------------------------------------------
+//
+//  DESCRIPTION
+//    all data related to system-dependent operations can be stored here
+//    unconditionally. the Go compiler takes care of identifying which of the
+//    symbols to export based on whichever platform we are targeting.
+//
+// =============================================================================
+
 package main
 
 const (
